a17aba0537e231a314b64d61a1303416644429da
Jovanne Sousa <jovanemaciel943@gmail.com>
2025-09-13 22:29:21 -0400

    fix: correção para deploy na vercel e imagem do public

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,19 @@ function styles(){
     .pipe(gulp.dest('./public/css/'));
 }
 
+function html () {
+  return gulp.src("src/**/*.html") 
+    .pipe(gulp.dest("public"));    
+}
+
 // function images() {
 //     return gulp.src('./src/images/**/*')
 //     .pipe(imagemin())
 //     .pipe(gulp.dest('./public/images'));
 // }
 
-exports.default = gulp.parallel(styles);
+exports.default = gulp.parallel(styles, html);
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles, html));
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 }
