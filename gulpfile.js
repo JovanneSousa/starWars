6daf419ca04855eb168bd3a7d3254c55d721a044
Jovanne Sousa <jovanemaciel943@gmail.com>
2025-09-13 22:22:00 -0400

    fix: correção para deploy na vercel

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify');
+// const imagemin = require('gulp-imagemin');
+// const uglify = require('gulp-uglify');
 
-function scripts (){
-    return gulp.src('./src/scripts/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/scripts/'))
-}
+// function scripts (){
+//     return gulp.src('./src/scripts/*.js')
+//         .pipe(uglify())
+//         .pipe(gulp.dest('./public/scripts/'))
+// }
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest('./public/css/'));
 }
 
 // function images() {
 //     return gulp.src('./src/images/**/*')
 //     .pipe(imagemin())
-//     .pipe(gulp.dest('./dist/images'));
+//     .pipe(gulp.dest('./public/images'));
 // }
 
-exports.default = gulp.parallel(styles, scripts);
+exports.default = gulp.parallel(styles);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
